4b1278a12b0f2f705d9b095d613b1df530d19b6e
tmichel- <tmichel-@student.42.com>
2023-07-06 13:28:48 +0200

    segfault quit fixed

--- srcs/free00.c
+++ srcs/free00.c
@@ -6,7 +6,7 @@
 /*   By: tmichel- <tmichel-@students.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 15:54:47 by sdanel            #+#    #+#             */
-/*   Updated: 2023/07/03 15:21:41 by tmichel-         ###   ########.fr       */
+/*   Updated: 2023/07/06 13:28:00 by tmichel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,6 @@ int	quit(t_glb *glb)
 {
 	mlx_destroy_window(glb->mlx.mlx, glb->mlx.window);
 	mlx_destroy_display(glb->mlx.mlx);
-	free(glb->mlx.img_wall);
-	free(glb->mlx.img_floor);
-	free(glb->mlx.img_player);
 	free(glb->mlx.mlx);
 	free_map(&glb->map);
 	exit(0);
